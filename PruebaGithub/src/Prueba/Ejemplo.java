f93759208d5df581a085a3112e0e41b35fbe6544
usuario1daw <aaviliz.21@gmail.com>
2022-11-10 21:54:30 +0100

    fff

---
+++ PruebaGithub/src/Prueba/Ejemplo.java
@@ -0,0 +1,29 @@
+package Prueba;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		//declaracion de variable 
+		//tipo - nombre = valor por defecto o inicial
+		
+		short num1 = 8;
+		int mes = 12;
+		long distancia = 30000;
+		
+		char letra = 'a';
+		float decimal = 1e10f;
+		double precio = 3500,95;
+		
+		boolean ganasDeIrma = true;
+		
+		System.out.println(num1);
+		System.out.println(mes);
+		System.out.println(distancia);
+		System.out.println(letra);
+		System.out.println(decimal);
+		System.out.println(precio);
+		System.out.println(ganasDeIrma);
+
+	}
+
+}
