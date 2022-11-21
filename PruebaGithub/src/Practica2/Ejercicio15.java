685f5e84188202b2aaba4d5bc30533e66b826b53
usuario1daw <aaviliz.21@gmail.com>
2022-11-21 20:49:15 +0100

    123

---
+++ PruebaGithub/src/Practica2/Ejercicio15.java
@@ -0,0 +1,25 @@
+package Practica2;
+
+public class Ejercicio15 {
+
+	public static void main(String[] args) {
+		
+		double interes = 2000;
+		double plazo = 2.75 / 100;
+		double retenido = 18.0 / 100;
+		double misIntereses;
+		double interesesRetenidos;
+		double misInteresesTotal;
+		
+		 misIntereses = interes * plazo / 2; 
+		 System.out.println(misIntereses);
+		 interesesRetenidos = misIntereses * retenido;
+		 System.out.println(interesesRetenidos);
+		 misInteresesTotal = misIntereses - interesesRetenidos;
+		 System.out.println(misInteresesTotal);
+		 
+		 
+		 
+	}
+
+}
