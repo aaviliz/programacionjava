6ddc71184f6340c54e5a6e8f1844d18f080d1f64
aaviliz <117848253+aaviliz@users.noreply.github.com>
2022-12-10 18:58:09 +0100

    f

---
+++ PruebaGithub/src/Practica3/Ejercicio7.java
@@ -0,0 +1,18 @@
+package Practica3EJ;
+
+public class Ej7 {
+
+	public static void main(String[] args) {
+
+		int i;
+		int contador = 0;
+		for (i=1;i<=100;i++){
+			if(i%2==1){
+				contador +=i;
+			}
+		}
+		System.out.println(contador);
+	
+	}
+
+}
