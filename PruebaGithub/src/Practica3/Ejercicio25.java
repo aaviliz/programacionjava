2467b694eabb13d19351106c3fd53c525761e2af
aaviliz <117848253+aaviliz@users.noreply.github.com>
2022-12-10 21:37:56 +0100

    f

---
+++ PruebaGithub/src/Practica3/Ejercicio25.java
@@ -0,0 +1,30 @@
+package Practica3EJ;
+
+import java.util.Scanner;
+public class Ej25 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	       int N, aux, inverso = 0, cifra;
+	        
+	           System.out.print("Introduce un número >= 10: ");
+	           N = sc.nextInt();
+	      
+	 
+	       aux = N;
+	       while (aux!=0){
+	           cifra = aux % 10;
+	           inverso = inverso * 10 + cifra;
+	           aux = aux / 10;
+	       }
+	
+	       if(N == inverso){
+	           System.out.println("Es capicua");
+	       }else{
+	           System.out.println("No es capicua");
+	       }
+
+	}
+
+}
