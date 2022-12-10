530bbf8c969d232d1f09518a2d9626fd6b1b92fd
aaviliz <117848253+aaviliz@users.noreply.github.com>
2022-12-10 19:33:11 +0100

    f

---
+++ PruebaGithub/src/Practica3/Ejercicio16.java
@@ -0,0 +1,20 @@
+package Practica3EJ;
+
+public class Ej16 {
+
+	public static void main(String[] args) {
+
+		int i=0;
+		
+		for (i=0;i<=100;i++) {
+			if (i%5==0) {
+			
+			}else {
+				System.out.println(i);
+			}
+		}
+		
+
+	}
+
+}
