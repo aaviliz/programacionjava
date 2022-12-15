80586eeee56354d097d720e4a0531e55471ac072
usuario1daw <aaviliz.21@gmail.com>
2022-12-15 18:54:17 +0100

    examen

--- PruevaEscrita/src/primeraevaluacion/Ejercicio4.java
+++ PruevaEscrita/src/primeraevaluacion/Ejercicio4.java
@@ -2,20 +2,9 @@ package primeraevaluacion;
 
 public class Ejercicio4 {
 
-		
-		public static void main(String[] args) {
-			
-			int[][] matriz = new int[20][20];
+	public static void main(String[] args) {
 			
 			
-			for(int i=0; i < matriz.length; i++) {
-				for(int j=0; j < matriz[i].length; j++) {
-					matriz[i][j] = (int) (Math.random() * 10);
-				}
-			}
-			
-			
-
 
 	}
 
